e45deb59f967652b6d82cb28409ae138342baf57
Ryo Sato <ryo.sato@datachain.jp>
2021-10-27 19:15:15 +0900

    modify config and favicon
    Signed-off-by: Ryo Sato <ryo.sato@datachain.jp>

--- docsrcs/yui-ibc-solidity/docusaurus.config.js
+++ docsrcs/yui-ibc-solidity/docusaurus.config.js
@@ -23,9 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          routeBasePath: '/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
