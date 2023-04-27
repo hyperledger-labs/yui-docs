8004dd970504ad6d0d52bb7ccc3103fc5d94b751
Ryo Sato <ryo.sato@datachain.jp>
2023-04-27 19:22:00 +0900

    using yui-ibc-solidity v0.3.3
    Signed-off-by: Ryo Sato <ryo.sato@datachain.jp>

--- samples/minitoken-besu-ethereum/chains/geth/run.sh
+++ samples/minitoken-besu-ethereum/chains/geth/run.sh
@@ -2,9 +2,9 @@
 
 /usr/local/bin/geth --password /root/geth.password \
   --unlock "0" --syncmode full \
-  --rpc --rpcvhosts "*" --rpcaddr "0.0.0.0" --rpcport "8545" --rpcapi web3,eth,net,personal,miner,txpool --rpccorsdomain '*' \
-  --ws --wsapi eth,net,web3,personal,txpool --wsaddr "0.0.0.0" --wsport "8546" --wsorigins '*' \
+  --http --http.vhosts "*" --http.addr "0.0.0.0" --http.port "8545" --http.api web3,eth,net,personal,miner,txpool --http.corsdomain '*' \
+  --ws --ws.api eth,net,web3,personal,txpool --ws.addr "0.0.0.0" --ws.port "8546" --ws.origins '*' \
   --datadir /root/.ethereum --networkid "2018" --nodiscover \
-  --mine --minerthreads 1 --gasprice "0" \
+  --mine --miner.threads 1 --miner.gasprice "0" --miner.etherbase "0xa89F47C6b463f74d87572b058427dA0A13ec5425" \
   --allow-insecure-unlock --nousb \
   $@
